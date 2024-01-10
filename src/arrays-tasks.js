7fb040ad44fe4086f2bee40f1891745f12dcdf30
Snischuk Maxim <m.snischuk@gmail.com>
2024-01-10 14:36:22 +0200

    feat: add tasks createNDimensionalArray, flattenArray, selectMany, calculateBalance, createChunks and generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -21,7 +21,10 @@
  *    getIntervalArray(3, 3) => [ 3 ]
  */
 function getIntervalArray(start, end) {
-  return Array.from({ length: end - start + 1 }, (e, i) => start + i);
+  return Array.from(
+    { length: end - start + 1 },
+    (item, index) => start + index
+  );
 }
 
 /**
@@ -40,7 +43,7 @@ function getIntervalArray(start, end) {
 function sumArrays(arr1, arr2) {
   return Array.from(
     { length: Math.max(arr1.length, arr2.length) },
-    (e, index) => (arr1[index] || 0) + (arr2[index] || 0)
+    (item, index) => (arr1[index] || 0) + (arr2[index] || 0)
   );
 }
 
@@ -263,8 +266,8 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  return Array(size).fill(n > 1 ? createNDimensionalArray(n - 1, size) : 0);
 }
 
 /**
@@ -278,8 +281,8 @@ function createNDimensionalArray(/* n, size */) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.flat(Infinity);
 }
 
 /**
@@ -295,8 +298,8 @@ function flattenArray(/* nestedArray */) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 /**
@@ -312,8 +315,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce(
+    (balance, [income, expense]) => balance + (income - expense),
+    0
+  );
 }
 
 /**
@@ -328,8 +334,12 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return Array(Math.ceil(arr.length / chunkSize))
+    .fill([])
+    .map((item, index) =>
+      arr.slice(index * chunkSize, (index + 1) * chunkSize)
+    );
 }
 
 /**
@@ -344,8 +354,8 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (item, index) => 2 * index + 1);
 }
 
 /**
